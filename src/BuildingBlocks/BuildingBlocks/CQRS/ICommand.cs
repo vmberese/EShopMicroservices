604fb2b9339a17bb18308d2b3aa8ac1cfed04e44
vmberese <vmberese@gmail.com>
2026-05-24 02:36:53 +0800

    Create abstraction on MediatR for CQRS

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommand.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildingBlocks.CQRS
+{
+    public interface ICommand : ICommand<Unit>
+    {
+    }
+
+    public interface ICommand<out TResponse> : IQuery<TResponse>
+        where TResponse : notnull
+    {
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+
+namespace Catalog.API.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+        : IRequest<CreateProductResult>;
+
+    public record CreateProductResult(Guid Id);
+
+    public class CreateProductHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+    {
+        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
