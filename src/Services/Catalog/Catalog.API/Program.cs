821aace4ee9759f1bc22f925da7181dea2af1f39
vmberese <vmberese@gmail.com>
2026-05-24 20:35:33 +0800

    Integrate Carter and Mapster for modular endpoints

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,9 +1,13 @@
+using Carter;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddCarter();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.MapCarter();
 
 app.Run();
