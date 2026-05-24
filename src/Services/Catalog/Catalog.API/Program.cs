901ff31d7f1915e8caccd39f68d2c78d45c2a3de
vmberese <vmberese@gmail.com>
2026-05-24 20:40:42 +0800

    Add MediatR service registration to DI container

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -2,6 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddCarter();
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(Program).Assembly));
 
 var app = builder.Build();
 
